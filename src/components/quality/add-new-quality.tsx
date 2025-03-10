92975489364f873e4eb6ddb8d8cffcb244aa8956
KRCS1-SYS <satyavan@krcs.in>
2025-03-10 15:53:49 +0530

    Update add-new-quality.tsx

--- src/components/quality/add-new-quality.tsx
+++ src/components/quality/add-new-quality.tsx
@@ -20,9 +20,9 @@ export default function AddNewQuality({ token, setReloadedTableData }: Props) {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="secondary">
+        /** <Button variant="secondary">
           Add <PlusCircle className="w-5 h-5" />
-        </Button>
+        </Button> **/
       </SheetTrigger>
       <SheetContent className="w-full">
         <SheetHeader>
